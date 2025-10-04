700852853789f6eddddcaf66bc06b084b113107c
utopy <30875147+utopyin@users.noreply.github.com>
2025-10-04 12:11:59 +0200

    fix: run prettier format

--- src/routes/WavyTitle.svelte
+++ src/routes/WavyTitle.svelte
@@ -1,43 +1,45 @@
 <script lang="ts">
-	import { cn } from "$lib/utils";
+	import { cn } from '$lib/utils';
 
 	let { text } = $props();
 </script>
 
 <p>
-    {#each [...text] as letter, i}
-        <span class="letter-container">
-            <span class={cn(
-                "letter",
+	{#each [...text] as letter, i}
+		<span class="letter-container">
+			<span
+				class={cn(
+					'letter',
 
-                "mt-0 mb-0",
+					'mt-0 mb-0',
 
-                "text-(--main-dark)",
-                "text-[90px]",
-                "font-[700]",
+					'text-(--main-dark)',
+					'text-[90px]',
+					'font-[700]',
 
-                "pointer-events-none",
+					'pointer-events-none'
 
-                // "translate-y-0", 
-                // "hover:translate-y-1000",
-                // "hover:text-red-400",
-                // "ease-(--ease-out-back) transition-transform duration-300",
-            )}>
-                {letter}
-            </span>
-        </span>
-    {/each}
+					// "translate-y-0",
+					// "hover:translate-y-1000",
+					// "hover:text-red-400",
+					// "ease-(--ease-out-back) transition-transform duration-300",
+				)}
+			>
+				{letter}
+			</span>
+		</span>
+	{/each}
 </p>
 
 <style>
-    .letter-container {
-        display: inline-block;
+	.letter-container {
+		display: inline-block;
 
-        height: auto;
-    }
+		height: auto;
+	}
 
-    .letter {
-        display: inline-block;
+	.letter {
+		display: inline-block;
 
 		text-shadow:
 			0px 2px 0px var(--main-dark-shad),
@@ -47,34 +49,34 @@
 			0px 10px 0px var(--main-dark-shad),
 			2px 15px 0 var(--transp-shad);
 
-        transform: translateY(0px);
+		transform: translateY(0px);
 
-        transition: 
-            transform 0.3s var(--ease-out-back),
-            text-shadow 0.3s var(--ease-out-back);
+		transition:
+			transform 0.3s var(--ease-out-back),
+			text-shadow 0.3s var(--ease-out-back);
 	}
 
-    .letter-container:hover .letter {
-        text-shadow:
+	.letter-container:hover .letter {
+		text-shadow:
 			0px 3.2px 0px var(--main-dark-shad),
 			0px 6.4px 0px var(--main-dark-shad),
 			0px 9.6px 0px var(--main-dark-shad),
 			0px 12.8px 0px var(--main-dark-shad),
 			0px 16px 0px var(--main-dark-shad),
 			4px 24px 0px var(--transp-shad);
-        
-        transform: translateY(-6px);
-    }
 
-    .letter-container:active .letter {
-        text-shadow:
+		transform: translateY(-6px);
+	}
+
+	.letter-container:active .letter {
+		text-shadow:
 			0px 1px 0px var(--main-dark-shad),
 			0px 2px 0px var(--main-dark-shad),
 			0px 3px 0px var(--main-dark-shad),
 			0px 4px 0px var(--main-dark-shad),
 			0px 7px 0px var(--main-dark-shad),
 			1px 10px 0px var(--transp-shad);
-        
-        transform: translateY(3px);
-    }
+
+		transform: translateY(3px);
+	}
 </style>
--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-	import Select from "./Select.svelte";
-
+	import Select from './Select.svelte';
 </script>
 
 <Select />
